b6f2f773c761196052c4ef549fd7b017d68f7902
Kaan Can Calkan <cancalkaniletisim@gmail.com>
2024-03-03 23:37:57 +0300

    Deploy patladı

--- ZHCM_UX_PRFL/webapp/controller/ErrorHandler.js
+++ ZHCM_UX_PRFL/webapp/controller/ErrorHandler.js
@@ -1,5 +1,168 @@
 /*
  * Copyright (C) 2009-2018 SAP SE or an SAP affiliate company. All rights reserved.
  */
-sap.ui.define(["sap/ui/base/Object","sap/m/MessageBox"],function(e,r){"use strict";function s(e){var r,s={sMessage:e,sDetails:null,aInnerErrors:[]};try{r=JSON.parse(e);if(r&&r.error){if(r.error.message&&r.error.message.value){s.sMessage=r.error.message.value}if(r.error.code){s.sDetails=r.error.code}if(r.error.innererror&&r.error.innererror.errordetails){s.aInnerErrors=r.error.innererror.errordetails}}}catch(r){try{var t=jQuery.parseXML(e)}catch(e){jQuery.sap.log.error(e)}if(t){s.sMessage=t.getElementsByTagName("message")[0].childNodes[0].nodeValue||t.documentElement;s.sDetails=t.getElementsByTagName("code")[0].childNodes[0].nodeValue;s.aInnerErrors=t.getElementsByTagName("errordetail")}else{s.sMessage=e}}return s}function t(e){var r=null,t=null;r=e.getParameters?e.getParameters():null;t=r?r.response||r:e;var o=t.responseText||t.body||t.response&&t.response.body||"";return s(o)}return e.extend("com.sedef.hcm.ux.myprofile.controller.ErrorHandler",{constructor:function(e){this._oResourceBundle=e.getModel("i18n").getResourceBundle();this._oComponent=e;this._oModel=e.getModel();this._sErrorText=this._oResourceBundle.getText("errorTitle");this._oModel.attachMetadataFailed(function(e){var r=e.getParameters();this._showMetadataError(r.response)},this);this._oModel.attachRequestFailed(this.onRequestFailed,this)},onRequestFailed:function(e){var r=t(e);this._showServiceError(r.sMessage,r.sDetails)},showErrorMsg:function(e,s){var o=t(e);r.show(o.sMessage,{icon:r.Icon.ERROR,title:this._oResourceBundle.getText("errorTitle"),details:o.sDetails,actions:r.Action.CLOSE,onClose:s,styleClass:this._oComponent.getContentDensityClass()})},_showMetadataError:function(e){r.error(this._sErrorText,{details:e,styleClass:this._oComponent.getContentDensityClass(),actions:[r.Action.RETRY,r.Action.CLOSE],onClose:function(e){if(e===r.Action.RETRY){this._oModel.refreshMetadata()}}.bind(this)})},_showServiceError:function(e,s,t){if(this._bMessageOpen){return}this._bMessageOpen=true;r.error(e,{title:t?t:this._sErrorText,details:s,styleClass:this._oComponent.getContentDensityClass(),actions:[r.Action.CLOSE],onClose:function(){this._bMessageOpen=false}.bind(this)})}})});
-//# sourceMappingURL=ErrorHandler.js.map
+sap.ui.define([
+	"sap/ui/base/Object",
+	"sap/m/MessageBox"
+], function(UI5Object, MessageBox) {
+	"use strict";
+
+	function fnExtractErrorContentFromResponse(sResponse) {
+		var errorJSON,
+			oError = {
+				sMessage: sResponse,
+				sDetails: null,
+				aInnerErrors: []
+			};
+
+		try {
+			// try to parse error as JSON-object first
+			errorJSON = JSON.parse(sResponse);
+
+			if (errorJSON && errorJSON.error) {
+				if (errorJSON.error.message && errorJSON.error.message.value) {
+					oError.sMessage = errorJSON.error.message.value;
+				}
+				if (errorJSON.error.code) {
+					oError.sDetails = errorJSON.error.code;
+				}
+				if (errorJSON.error.innererror && errorJSON.error.innererror.errordetails) {
+					oError.aInnerErrors = errorJSON.error.innererror.errordetails;
+				}
+			}
+
+		} catch (e) {
+			// xml is parsed using jQuery
+			try {
+				var xmlDoc = jQuery.parseXML(sResponse);
+			} catch (f) {
+				jQuery.sap.log.error(f);
+			}
+
+			if (xmlDoc) {
+				oError.sMessage = xmlDoc.getElementsByTagName("message")[0].childNodes[0].nodeValue || xmlDoc.documentElement;
+				oError.sDetails = xmlDoc.getElementsByTagName("code")[0].childNodes[0].nodeValue;
+				oError.aInnerErrors = xmlDoc.getElementsByTagName("errordetail");
+			} else {
+				// Just in case that the Error from request could not be parsed
+				oError.sMessage = sResponse;
+			}
+		}
+		return oError;
+	}
+
+	function fnParseError(oEvent) {
+		var oParameters = null,
+			oResponse = null;
+
+		// "getParameters": for the case of catching oDataModel "requestFailed" event
+		oParameters = oEvent.getParameters ? oEvent.getParameters() : null;
+		// "oParameters.response": V2 interface, response object is under the getParameters()
+		// "oParameters": V1 interface, response is directly in the getParameters()
+		// "oEvent" for the case of catching request "onError" event
+		oResponse = oParameters ? (oParameters.response || oParameters) : oEvent;
+		var responseContent = oResponse.responseText || oResponse.body || (oResponse.response && oResponse.response.body) || ""; //"onError" Event: V1 uses response and response.body
+		return fnExtractErrorContentFromResponse(responseContent);
+	}
+
+	return UI5Object.extend("com.sedef.hcm.ux.myprofile.controller.ErrorHandler", {
+
+		/**
+		 * Handles application errors by automatically attaching to the model events and displaying errors when needed.
+		 * @class
+		 * @param {sap.ui.core.UIComponent} oComponent reference to the app's component
+		 * @public
+		 * @alias hcm.fab.employeelookup.controller.ErrorHandler
+		 */
+		constructor: function(oComponent) {
+			this._oResourceBundle = oComponent.getModel("i18n").getResourceBundle();
+			this._oComponent = oComponent;
+			this._oModel = oComponent.getModel();
+			this._sErrorText = this._oResourceBundle.getText("errorTitle");
+
+			this._oModel.attachMetadataFailed(function(oEvent) {
+				var oParams = oEvent.getParameters();
+				this._showMetadataError(oParams.response);
+			}, this);
+
+			this._oModel.attachRequestFailed(this.onRequestFailed, this);
+		},
+
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
+
+		/**
+		 * Event Handler for Request Fail event
+		 * The user can try to refresh the metadata.
+		 * @param {object} oEvent an event containing the response data
+		 * @private
+		 */
+		onRequestFailed: function(oEvent) {
+			var oError = fnParseError(oEvent);
+			// deal with main error message from response
+			this._showServiceError(oError.sMessage, oError.sDetails);
+		},
+
+		/**
+		 * Shows a {@link sap.m.MessageBox}.
+		 * @param {object} oEvent an event containing the response data, {function} a function that is executed on closing the MessageBox
+		 * @param {function} fnOnClose a function that gets executed once the Message box is closed
+		 * @public
+		 */
+		showErrorMsg: function(oEvent, fnOnClose) {
+			var oErrorDetails = fnParseError(oEvent);
+			MessageBox.show(oErrorDetails.sMessage, {
+				icon: MessageBox.Icon.ERROR,
+				title: this._oResourceBundle.getText("errorTitle"),
+				details: oErrorDetails.sDetails,
+				actions: MessageBox.Action.CLOSE,
+				onClose: fnOnClose,
+				styleClass: this._oComponent.getContentDensityClass()
+			});
+		},
+
+		/**
+		 * Shows a {@link sap.m.MessageBox} when the metadata call has failed.
+		 * The user can try to refresh the metadata.
+		 * @param {string} sDetails a technical error to be displayed on request
+		 * @private
+		 */
+		_showMetadataError: function(sDetails) {
+			MessageBox.error(
+				this._sErrorText, {
+					details: sDetails,
+					styleClass: this._oComponent.getContentDensityClass(),
+					actions: [MessageBox.Action.RETRY, MessageBox.Action.CLOSE],
+					onClose: function(sAction) {
+						if (sAction === MessageBox.Action.RETRY) {
+							this._oModel.refreshMetadata();
+						}
+					}.bind(this)
+				}
+			);
+		},
+
+		/**
+		 * Shows a {@link sap.m.MessageBox} when a service call has failed.
+		 * Only the first error message will be display.
+		 * @param {string} sDetails a technical error to be displayed on request
+		 * @private
+		 */
+		_showServiceError: function(sErrorText, sErrorDetails, sTitleText) {
+			if (this._bMessageOpen) {
+				return;
+			}
+			this._bMessageOpen = true;
+			MessageBox.error(sErrorText, {
+				title: sTitleText ? sTitleText : this._sErrorText,
+				details: sErrorDetails,
+				styleClass: this._oComponent.getContentDensityClass(),
+				actions: [MessageBox.Action.CLOSE],
+				onClose: function() {
+					this._bMessageOpen = false;
+				}.bind(this)
+			});
+		}
+	});
+});
