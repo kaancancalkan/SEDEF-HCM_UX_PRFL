b30cad95ccce3680f2a612b5ab760133c4ed7f4c
Kaan Can Calkan <cancalkaniletisim@gmail.com>
2024-03-03 03:20:28 +0300

    DBG Part1

--- ZHCM_UX_PRFL/webapp/blocks/CareerProgressBlock.js
+++ ZHCM_UX_PRFL/webapp/blocks/CareerProgressBlock.js
@@ -1,5 +1,24 @@
 /*
  * Copyright (C) 2009-2018 SAP SE or an SAP affiliate company. All rights reserved.
  */
-sap.ui.define(["sap/uxap/BlockBase"],function(e){"use strict";return e.extend("com.sedef.hcm.ux.myprofile.blocks.CareerProgressBlock",{metadata:{views:{Collapsed:{viewName:"com.sedef.hcm.ux.myprofile.blocks.CareerProgressBlock",type:"XML"},Expanded:{viewName:"com.sedef.hcm.ux.myprofile.blocks.CareerProgressBlock",type:"XML"}},events:{}}})});
-//# sourceMappingURL=CareerProgressBlock.js.map
+sap.ui.define([
+	"sap/uxap/BlockBase"
+], function(BlockBase) {
+	"use strict";
+
+	return BlockBase.extend("com.sedef.hcm.ux.myprofile.blocks.CareerProgressBlock", {
+		metadata: {
+			views: {
+				Collapsed: {
+					viewName: "com.sedef.hcm.ux.myprofile.blocks.CareerProgressBlock",
+					type: "XML"
+				},
+				Expanded: {
+					viewName: "com.sedef.hcm.ux.myprofile.blocks.CareerProgressBlock",
+					type: "XML"
+				}
+			},
+			events: {}
+		}
+	});
+});
